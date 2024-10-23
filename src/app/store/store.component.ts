9a0c4e97db97a9f74529beb97082f4f224ef1bd0
Govind Tandon <govindtandon00@gmail.com>
2024-10-23 10:55:32 +0530

    sidebar

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,13 +8,26 @@ import { ProductRepository } from "../model/product.repository";
 })
 
 export class StoreComponent {
+
+    /* selectedCategory property is assigned the user’s choice of category (where undefined
+means all categories) and is used in the updateData method as an argument to the getProducts method
+    */
+    selectedCategory: string | undefined;
     constructor(private repository: ProductRepository) { }
 
     get Products(): Product[]{
-        return this.repository.getProducts();
+        return this.repository.getProducts(this.selectedCategory);
     }
 
     get categories(): string[]{
         return this.repository.getCategories();
+    }
+
+    /* 
+    changeCategory method brings these two members together 
+in a method that can be invoked when the user makes a category selection. 
+    */
+    changeCategory(newCategory ?: string){
+        this.selectedCategory = newCategory;
     }
 }
